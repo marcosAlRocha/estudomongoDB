9e6e398a56cd17c5ba9c8bccee3c241a8e564879
Marcos Al. Rocha <marcosar@gmail.com>
2020-01-14 16:46:21 -0200

    Instanciando uma base de dados

---
+++ src/main/java/com/mrxatecnologia/springmongo/config/Instantiation.java
@@ -0,0 +1,30 @@
+package com.mrxatecnologia.springmongo.config;
+
+import com.mrxatecnologia.springmongo.domain.User;
+import com.mrxatecnologia.springmongo.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Arrays;
+
+@Configuration
+public class Instantiation implements CommandLineRunner {
+
+    @Autowired
+    private UserRepository userRepository;
+
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        userRepository.deleteAll();
+
+        User maria = new User(null, "Maria Brown", "maria@gmail.com");
+        User alex = new User(null, "Alex Green", "alex@gmail.com");
+        User bob = new User(null, "Bob Grey", "bob@gmail.com");
+
+        userRepository.saveAll(Arrays.asList(maria, alex, bob));
+
+    }
+}
